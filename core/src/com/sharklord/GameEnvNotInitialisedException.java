3fb977fd8ab455ea18d47a0946515648bb662972
Filip Binkiewicz <filip.binkiewicz@gmail.com>
2016-07-06 12:01:33 +0200

    There's a shark - and it jumps!

---
+++ core/src/com/sharklord/GameEnvNotInitialisedException.java
@@ -0,0 +1,6 @@
+package com.sharklord;
+
+/**
+ * Created by filip on 06.07.16.
+ */
+public class GameEnvNotInitialisedException extends Exception {}
--- core/src/com/sharklord/SharkLordGame.java
+++ core/src/com/sharklord/SharkLordGame.java
@@ -3,35 +3,47 @@ package com.sharklord;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.math.Vector3;
-
-
 
 public class SharkLordGame extends ApplicationAdapter implements InputProcessor {
+	SpriteBatch			batch;
+	Texture				sharkImage;
+	Shark				shark;
+
+	private void drawShark() {
+
+	}
 
 	@Override
 	public void create() {
 		Gdx.input.setInputProcessor(this);
+		batch = new SpriteBatch();
+		shark = Shark.getInstance();
+		sharkImage = new Texture(Gdx.files.internal("shark_placeholder.png"));
 	}
 
 	@Override
 	public void render() {
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		float dt = Gdx.graphics.getDeltaTime();
+		shark.update(dt);
+		batch.draw(sharkImage, shark.getX(), shark.getY(), shark.width(), shark.height());
+		batch.end();
 	}
 
 	@Override
 	public void dispose() {
+		batch.dispose();
 	}
 
 	@Override
 	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-		return false;
+		shark.jump();
+		return true;
 	}
 	@Override
 	public boolean keyDown(int keycode) {
